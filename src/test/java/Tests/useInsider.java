d3f547efa2a97ebe39bc9a561cb8633949805b70
Asmuftu <firex8@icloud.com>
2023-12-19 18:19:28 +0300

    first commit

---
+++ src/test/java/Tests/useInsider.java
@@ -0,0 +1,67 @@
+package Tests;
+
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.InsiderPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
+public class useInsider {
+
+    //● Test Automation
+    //1. Visit https://useinsider.com/ and check Insider home page is opened or not
+    //2. Select “Company” menu in navigation bar, select “Careers” and check Career page, its
+    //Locations, Teams and Life at Insider blocks are opened or not
+    //3. Click “See All Teams”, select Quality Assurance, click “See all QA jobs”, filter jobs by
+    //Location - Istanbul, Turkey and department - Quality Assurance, check presence of
+    //jobs list
+    //4. Check that all jobs’ Position contains “Quality Assurance”, Department contains
+    //“Quality Assurance”, Location contains “Istanbul, Turkey” and “Apply Now” button
+    //5. Click “Apply Now” button and check that this action redirects us to Lever Application
+    //form page
+    //- Test case should be written using any programming language and framework
+    //(Python or Java + Selenium would be preferable)
+    //- No BDD(Cucumber, Quantum, Codeception, etc.) frameworks are allowed
+    //- Screenshot should be taken If test fails one of steps
+    //- Test case should be able to run in Chrome and Firefox browsers and ensure that the
+    //browser is parametrically changeable.
+    //- Test code should fully meet POM requirements
+
+    @Test
+    public void insiderTesti(){
+        //1. Visit https://useinsider.com/ and check Insider home page is opened or not
+        Driver.getDriver().get(ConfigReader.getProperty("insiderUrl"));
+        String expectedUrl = "https://useinsider.com/";
+        String actualUrl = Driver.getDriver().getCurrentUrl();
+        Assert.assertEquals(actualUrl,expectedUrl);
+
+        //2. Select “Company” menu in navigation bar, select “Careers” and check Career page, its
+        //Locations, Teams and Life at Insider blocks are opened or not
+        InsiderPage insiderPage = new InsiderPage();
+        insiderPage.cookiesAcceptElement.click();
+        insiderPage.companyDdmElement.click();
+        insiderPage.careerElement.click();
+
+        Assert.assertTrue(insiderPage.FindYourCallingText.isDisplayed());
+        Assert.assertTrue(insiderPage.OurLocationsText.isDisplayed());
+        Assert.assertTrue(insiderPage.LifeAtInsiderText.isDisplayed());
+
+        //3. Click “See All Teams”, select Quality Assurance, click “See all QA jobs”, filter jobs by
+        //Location - Istanbul, Turkey and department - Quality Assurance, check presence of
+        //jobs list
+        insiderPage.seeAllTeamsButton.click();
+        insiderPage.QAButton.click();
+        insiderPage.seeAllQAJobsButton.click();
+        Select select = new Select(insiderPage.locationFilterDdm);
+        select.selectByVisibleText("Istanbul, Turkey");
+        Assert.assertTrue(insiderPage.firstJobElement.isDisplayed());
+
+
+
+        ReusableMethods.bekle(2);
+        Driver.quitDriver();
+    }
+}
+
---
+++ src/test/java/pages/InsiderPage.java
@@ -0,0 +1,48 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+import java.nio.file.Watchable;
+
+public class InsiderPage {
+
+    public InsiderPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(id = "wt-cli-accept-all-btn")
+    public WebElement cookiesAcceptElement;
+
+    @FindBy(xpath = "(//a[@id='navbarDropdownMenuLink'])[5]")
+    public WebElement companyDdmElement;
+
+    @FindBy(xpath = "//*[text()='Careers']")
+    public WebElement careerElement;
+
+    @FindBy(xpath = "(//h3[@class='category-title-media'])[1]")
+    public WebElement FindYourCallingText;
+
+    @FindBy(xpath = "(//h3[@class='category-title-media ml-0'])[1]")
+    public WebElement OurLocationsText;
+
+    @FindBy(xpath = "(//h2[@class='elementor-heading-title elementor-size-default'])[2]")
+    public WebElement LifeAtInsiderText;
+
+    @FindBy(xpath = "//a[@class='btn btn-outline-secondary rounded text-medium mt-5 mx-auto py-3 loadmore']")
+    public WebElement seeAllTeamsButton;
+
+    @FindBy(xpath = "(//h3[@class='text-center mb-4 mb-xl-5'])[12]")
+    public WebElement QAButton;
+
+    @FindBy(xpath = "//*[text()='See all QA jobs']")
+    public WebElement seeAllQAJobsButton;
+
+    @FindBy(xpath = "(//span[@class='select2-selection__rendered'])[1]")
+    public WebElement locationFilterDdm;
+
+    @FindBy(xpath = "(//div[@class='position-list-item-wrapper bg-light'])[1]")
+    public WebElement firstJobElement;
+}
